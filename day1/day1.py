4c6dc9ff922ca7ca80f634c1066111f765c46911
Loren Stallcop <loren.stallcop@gmail.com>
2021-12-25 19:59:40 -0600

    fix day1, solve day2, start day3

--- day1/day1.py
+++ day1/day1.py
@@ -10,16 +10,23 @@ print(sonar_input_data[0])
 for item in sonar_input_data:
     sonar_data_clean.append(item.strip())
 
-increase_count=0
+def method1():
+    increase_count=0
+    for count,value in enumerate(sonar_data_clean):
+        if count > 0:
+            print(count)
+            print(value)
+            if sonar_data_clean[count] > sonar_data_clean[count-1]:
+                print(increase_count)
+                increase_count=increase_count+1
+    return increase_count
 
-for count,value in enumerate(sonar_data_clean):
-    if count > 0:
-        print(count)
-        print(value)
-        if sonar_data_clean[count] > sonar_data_clean[count-1]:
-            print(increase_count)
-            increase_count=increase_count+1
+def method2():
+    compare_list = [currentNum < nextNum for currentNum, nextNum in zip(sonar_data_clean,sonar_data_clean[1:])]
+
+    return sum(compare_list)
 
 print(len(sonar_data_clean))
 
-print('The depth increase the following number of times: ',increase_count)
+print('The depth increase the following number of times: ',method1())
+print('The depth increase number using method 2: ', method2())
--- day1/day1_part2.py
+++ day1/day1_part2.py
@@ -10,18 +10,33 @@ print(sonar_input_data[0])
 for item in sonar_input_data:
     sonar_data_clean.append(item.strip())
 
-increase_count=0
+for item in sonar_data_clean:
+    int(item)
 
-for count,value in enumerate(sonar_data_clean):
-    if count > 2:
-        print(count)
-        print(value)
-        current_window = sonar_data_clean[count] + sonar_data_clean[count-1] + sonar_data_clean[count-2]
-        previous_window = sonar_data_clean[count-1] + sonar_data_clean[count-2] + sonar_data_clean[count-3]
-        if current_window > previous_window:
-            print(increase_count)
-            increase_count=increase_count+1
+def method1():
+    increase_count=0
+    for count,value in enumerate(sonar_data_clean):
+        if count > 2:
+            print(count)
+            print(value)
+            current_window = sonar_data_clean[count] + sonar_data_clean[count-1] + sonar_data_clean[count-2]
+            previous_window = sonar_data_clean[count-1] + sonar_data_clean[count-2] + sonar_data_clean[count-3]
+            if current_window > previous_window:
+                print(increase_count)
+                increase_count=increase_count+1
+    return increase_count
+
+def method2():
+    debug_list = [zip(sonar_data_clean,sonar_data_clean[1:],sonar_data_clean[2:])]
+    print(debug_list)
+    sum_list = [sum([int(x) for x in sonar_tuple]) for sonar_tuple in zip(sonar_data_clean,sonar_data_clean[1:],sonar_data_clean[2:])]
+    #sum_list = [sum(sonar_tuple) for sonar_tuple in zipped_list]
+    compare_list = [currentNum < nextNum for currentNum, nextNum in zip(sum_list,sum_list[1:])]
+
+    return sum(compare_list)
 
 print(len(sonar_data_clean))
 
-print('The depth increase the following number of times: ',increase_count)
+print('The depth increase the following number of times: ',method1())
+print('The depth increase according to our second calculation method: ',method2())
+
